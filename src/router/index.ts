2e25c44c41113e633723a78fbd5cef1c6faed7e5
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-06-19 00:11:25 +0300

    Move router

---
+++ src/router/index.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import userController from "../controllers/user-controller";
+import { body } from "express-validator";
+import authMiddleware from "../middlewares/auth-middleware";
+
+import dictionaryController from "../controllers/dictionaryController";
+import vocabularyController from "../controllers/vocabularyController";
+
+const router = Router();
+
+router.post(
+  "/registration",
+  body("email").isEmail(),
+  body("password").isLength({ min: 6, max: 32 }),
+  userController.registration
+);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
+router.get("/activate/:link", userController.activate);
+router.get("/refresh", userController.refresh);
+router.get("/users", authMiddleware, userController.getUsers);
+
+router.get(
+  "/dictionaryary/search",
+  authMiddleware,
+  dictionaryController.search
+);
+router.get(
+  "/dictionaryary/fake-words",
+  authMiddleware,
+  dictionaryController.fakeWords
+);
+router.get(
+  "/dictionaryary/fake-translations",
+  authMiddleware,
+  dictionaryController.fakeTranslations
+);
+
+router.get("/vocabulary", authMiddleware, vocabularyController.getVocabulary);
+router.post("/vocabulary/add", authMiddleware, vocabularyController.addWord);
+router.patch("/vocabulary/update", authMiddleware, vocabularyController.update);
+router.put("/vocabulary/updates", authMiddleware, vocabularyController.updates);
+
+export default router;
