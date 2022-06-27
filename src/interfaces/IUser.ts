a4e0470241e8c5256823b4e95034b26b6db59342
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-06-28 00:34:42 +0300

    Fix id key

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,7 +1,7 @@
 import { Document, Types } from "mongoose";
 
 export interface IUser extends Document {
-  id: Types.ObjectId;
+  _id: Types.ObjectId;
   email: string;
   password: string;
   activationLink?: string;
