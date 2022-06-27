4b083cbb281cb0e1eb80c37bb2e1b9713f7df204
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-06-28 00:33:43 +0300

    Add voc dto

---
+++ src/dtos/vocabularyDto.ts
@@ -0,0 +1,26 @@
+import { Types } from "mongoose";
+import { IVocabularyModel, WordStatus } from "../models/vocabularyModel";
+
+export class VocabularyWordDto {
+  id: Types.ObjectId;
+  word: string;
+  translation: string;
+  note: string;
+  status: WordStatus;
+  lastSuccessful: string;
+  attempts: number;
+  successfulAttempts: number;
+  active: boolean;
+
+  constructor(model: IVocabularyModel) {
+    this.id = model._id;
+    this.word = model.word;
+    this.translation = model.translation;
+    this.note = model.note;
+    this.status = model.status;
+    this.lastSuccessful = model.lastSuccessful;
+    this.attempts = model.attempts;
+    this.successfulAttempts = model.successfulAttempts;
+    this.active = model.active;
+  }
+}
