3bc26ff2070e195328b78b336ba1f62388b8e615
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-07-17 00:39:15 +0300

    Use only one id field

--- src/models/vocabularyModel.ts
+++ src/models/vocabularyModel.ts
@@ -8,7 +8,7 @@ export enum WordStatus {
 }
 
 export interface IVocabularyModel {
-  _id: Types.ObjectId;
+  id: Types.ObjectId;
   user: Types.ObjectId;
   word: string;
   translation: string;
@@ -20,21 +20,35 @@ export interface IVocabularyModel {
   active: boolean;
 }
 
-const VocabularySchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  word: { type: String, required: true },
-  translation: { type: String, required: true },
-  note: { type: String },
-  status: {
-    type: String,
-    required: true,
-    enum: ["unknown", "study", "learned"],
-    default: "unknown",
+const VocabularySchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    word: { type: String, required: true },
+    translation: { type: String, required: true },
+    note: { type: String },
+    status: {
+      type: String,
+      required: true,
+      enum: ["unknown", "study", "learned"],
+      default: "unknown",
+    },
+    lastSuccessful: { type: Date, default: null },
+    attempts: { type: Number, default: 0 },
+    successfulAttempts: { type: Number, default: 0 },
+    active: { type: Boolean, default: true },
   },
-  lastSuccessful: { type: Date, default: null },
-  attempts: { type: Number, default: 0 },
-  successfulAttempts: { type: Number, default: 0 },
-  active: { type: Boolean, default: true },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const virtual = VocabularySchema.virtual("id");
+virtual.set(function (value) {
+  this._id = value;
+});
+virtual.get(function () {
+  return this._id;
 });
 
 export default model<IVocabularyModel>(
