f73a65cd7c8044ec444e4dc2824d22c141ca6ee7
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-07-17 00:33:29 +0300

    Update names

--- src/controllers/dictionaryController.js
+++ src/controllers/dictionaryController.js
@@ -1,4 +1,4 @@
-import userService from "../service/user-service";
+import dictionaryService from "../service/dictionaryService";
 import { validationResult } from "express-validator";
 import ApiError from "../exeptions/api-error";
 
@@ -6,14 +6,16 @@ class DictionaryController {
   async search(req, res, next) {
     try {
       // TODO cheack on norm str
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return next(ApiError.BadRequest("Error validation", errors));
-      }
-      const { search } = req.query;
-      const searchResluts = await dictionaryService.search(search);
+      // const errors = validationResult(req);
+      // if (!errors.isEmpty()) {
+      //   return next(ApiError.BadRequest("Error validation", errors));
+      // }
 
-      console.log("======== search", req, searchResluts);
+      const searchString = req.query.word;
+      console.log("DictionaryController search ", searchString, req.query);
+      const searchResluts = await dictionaryService.search(searchString);
+
+      console.log("DictionaryController ======== []", searchResluts.length);
 
       return res.json(searchResluts);
     } catch (error) {
@@ -24,9 +26,10 @@ class DictionaryController {
   async fakeWords(req, res, next) {
     try {
       // TODO cheack on norm str
-      const { search } = req.query;
+      const searchString = req.query.word;
 
-      const searchResluts = await dictionaryService.fakeWords(search);
+      console.log("======== fakeWords", searchString);
+      const searchResluts = await dictionaryService.fakeWords(searchString);
       console.log("======== fakeWords", searchResluts);
       return res.json(searchResluts);
     } catch (error) {
@@ -36,9 +39,12 @@ class DictionaryController {
 
   async fakeTranslations(req, res, next) {
     try {
-      const { search } = req.query;
-      const searchResluts = await dictionaryService.fakeTranslations(search);
-      console.log("======== fakeTranslations", search, searchResluts);
+      const searchString = req.query.word;
+
+      const searchResluts = await dictionaryService.fakeTranslations(
+        searchString
+      );
+      console.log("======== fakeTranslations", searchString, searchResluts);
       res.json(searchResluts);
     } catch (error) {
       next(error);
