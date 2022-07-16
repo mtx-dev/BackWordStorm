4afcf09805d0eec012ce94db0c62c98ebbea974f
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-07-17 00:36:01 +0300

    Update dto

--- src/dtos/user-dto.ts
+++ src/dtos/user-dto.ts
@@ -1,14 +1,22 @@
 import { Types } from "mongoose";
-import { IUser } from "../interfaces/IUser";
+import { ISettings, IUser } from "../interfaces/IUser";
 
 export class UserDto {
   id: Types.ObjectId;
   email: string;
   isActivated: boolean;
+  settings: ISettings;
 
   constructor(model: IUser) {
     this.id = model._id;
     this.email = model.email;
     this.isActivated = model.isActivated;
+    this.settings = model.settings;
   }
+
+  public toPayload = () => ({
+    id: this.id,
+    email: this.email,
+    isActivated: this.isActivated,
+  });
 }
